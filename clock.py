ddc01bf865c7cf439ac52987e1180848180e9d37
Daniel Barclay <barclaysd@me.com>
2019-01-20 01:10:22 +0000

    Smoother transition of hour and minute hands. Digital time displayed below analogue clock

--- clock.py
+++ clock.py
@@ -50,22 +50,22 @@ class ClockHands(turtle.Turtle):
         self.penup()
         self.hideturtle()
 
-    def draw_hour_hand(self, h):
+    def draw_hour_hand(self, h, m):
         self.penup()
         self.setposition(0, 0)
         self.color('#fff')
         self.setheading(90)
-        angle = (h / 12) * 360
+        angle = ((h / 12) * 360) + ((m / 60) * 30)
         self.rt(angle)
         self.pendown()
         self.fd(100)
 
-    def draw_minute_hand(self, m):
+    def draw_minute_hand(self, m, s):
         self.penup()
         self.setposition(0, 0)
         self.color('#006666')
         self.setheading(90)
-        angle = (m / 60) * 360
+        angle = ((m / 60) * 360) + ((s/60) * 6)
         self.rt(angle)
         self.pendown()
         self.fd(175)
@@ -81,6 +81,27 @@ class ClockHands(turtle.Turtle):
         self.fd(125)
 
 
+class DigitalTime(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.hideturtle()
+        self.speed(0)
+        self.color("#fff")
+        self.setposition(0, -275)
+
+    def display_time(self, h, m, s):
+        self.clear()
+        self.write("{} : {} : {}".format(self.format_time(h), self.format_time(m), self.format_time(s)), False,
+                   align="center", font=("Verdana", 28, "normal"))
+
+    def format_time(self, number):
+        if number < 10:
+            return '0' + str(number)
+        else:
+            return str(number)
+
+
 class Time:
     def __init__(self):
         self.hour = time.localtime().tm_hour
@@ -94,7 +115,7 @@ class Time:
 # class instances
 clock_outline = ClockOutline()
 clock_hands = ClockHands()
-
+digital_time = DigitalTime()
 # draw clock outline
 clock_outline.draw_clock()
 clock_outline.draw_hours()
@@ -104,8 +125,9 @@ while True:
     # class instances
     currentTime = Time()
     # draw clock hands
-    clock_hands.draw_hour_hand(currentTime.hour)
-    clock_hands.draw_minute_hand(currentTime.minute)
+    clock_hands.draw_hour_hand(currentTime.hour, currentTime.minute)
+    clock_hands.draw_minute_hand(currentTime.minute, currentTime.second)
     clock_hands.draw_seconds_hand(currentTime.second)
+    digital_time.display_time(currentTime.hour, currentTime.minute, currentTime.second)
     wn.update()
     clock_hands.clear()
